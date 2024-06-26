006d70fbd540b97abf6a4289c3280a077dbc3630
Andriikryl <andreyk557@gmail.com>
2024-06-26 12:27:45 +0300

    add todoLeft component

---
+++ src/components/TodosLeft.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let incompleteTodos: number;
+</script>
+
+<h4 class="flowers__count">
+  {incompleteTodos}
+  {incompleteTodos === 1 ? "flower" : "flowers"} active
+</h4>
+
+<style>
+  .flowers__count {
+    font-family: var(--font-family);
+    font-weight: 400;
+    font-size: 24px;
+    color: var(--black);
+    text-align: center;
+  }
+</style>
