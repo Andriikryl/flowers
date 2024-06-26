e1da0f43e862ec92b28189703e3ecfe1c84baa62
Andriikryl <andreyk557@gmail.com>
2024-06-26 12:29:52 +0300

    add sides

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+  import Todos from "./components/Todos.svelte";
 </script>
 
-<main>
-  <div>adasd</div>
+<main class="wrapper | wflowers__main">
+  <Todos />
 </main>
 
-<style></style>
+<style>
+  .wflowers__main {
+    min-height: 100dvh;
+    background-image: url(/bg.jpeg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+</style>
